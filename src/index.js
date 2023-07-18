6567a6f0ed7847154e53009f6c7e90ed38938189
wiictor <wiictor96@gmail.com>
2023-07-18 18:05:20 +0200

    Redux persist and devtools installation

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
 
 import "./index.scss";
 import { StyleSheetManager } from "styled-components";
@@ -18,9 +19,11 @@ ReactDOM.render(
       disabledVendorPrefixes={false}
     >
       <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </PersistGate>
       </Provider>
     </StyleSheetManager>
   </React.StrictMode>,
